56508d6aab426c87083feb03dfa7642212a82a6d
TerracottaPie191514 <martijnmelissen@hotmail.com>
2023-05-21 19:25:31 +0200

    Initial commit

---
+++ load.R
@@ -0,0 +1,110 @@
+
+
+##  For Martijn Melissen - April 2023
+### ImportData subset n=120 field study, n=6 per farm
+#### n=120 METAGENOMIC and 16S
+
+#### background:  https://mibwurrepo.github.io/Microbial-bioinformatics-introductory-course-Material-2018/introduction.html   #Introbackground
+####              https://microbiome.github.io/OMA/    # chapter 10, clustering
+#### Preprocces NG-tax:
+####              https://www.frontiersin.org/articles/10.3389/fgene.2019.01366/full
+####              https://f1000research.com/articles/5-1791
+
+#### Load packages
+library(microbiome) # data analysis and visualisation
+library(phyloseq) # also the basis of data object. Data analysis and visualisation
+library(microbiomeutilities) # some utility tools
+library(RColorBrewer) # nice color options
+library(ggpubr) # publication quality figures, based on ggplot2
+library(DT) # interactive tables in html and markdown
+library(data.table) # alternative to data.frame
+library(dplyr) # data handling
+library(RColorBrewer)
+library(pheatmap)
+library(readr)
+library(picante)
+library(nlme)
+
+### create phyloseq object, https://joey711.github.io/phyloseq/
+pseq <- read_phyloseq(otu.file= "ASV.biom1",
+                      taxonomy.file = NULL,
+                      metadata.file = "MetaData.csv",
+                      type="biom", sep =";" )
+
+
+treefile <- read_tree("all_asvTREE.tree")
+ps <-merge_phyloseq(pseq, treefile)
+ps
+
+#> ps
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 6249 taxa and 180 samples ]
+#sample_data() Sample Data:       [ 180 samples by 26 sample variables ]
+#tax_table()   Taxonomy Table:    [ 6249 taxa by 6 taxonomic ranks ]
+#phy_tree()    Phylogenetic Tree: [ 6249 tips and 6248 internal nodes ]
+
+### 180 samples
+
+### overview data
+datatable(tax_table(ps))
+
+### remove some contamination
+subset <- subset_taxa(ps, Domain !="NA")
+subset <- subset_taxa(subset,Family !="f__Mitochondria=*")
+subset <- subset_taxa(subset,Family !="f__Mitochondria")
+subset <- subset_taxa(subset, Order !="o__Chloroplast")
+#do you know why? filter out plant data and eukaryotes? food ? or algae and stuff living? aren't they cyanobacteria?
+subset <- subset_taxa(subset, Domain!="k__Archaea") # can be discussed about it
+
+### remove taxa with zeros
+subset <- prune_taxa(taxa_sums(subset) > 0, subset)
+
+### subset phyloseq object n=120 metagenomics data
+subsetG  <- subset_samples(subset,  Metagenomics == "yes" )    #n=120
+subsetG <- prune_taxa(taxa_sums(subsetG) > 0, subsetG)
+subsetG
+
+datatable(tax_table(subsetG))
+
+#subsetG
+#phyloseq-class experiment-level object
+#otu_table()   OTU Table:         [ 1536 taxa and 120 samples ]
+#sample_data() Sample Data:       [ 120 samples by 26 sample variables ]
+#tax_table()   Taxonomy Table:    [ 1536 taxa by 6 taxonomic ranks ]
+#phy_tree()    Phylogenetic Tree: [ 1536 tips and 1535 internal nodes ]
+
+rank_names(subsetG)
+get_taxa_unique(subsetG, "Phylum")
+sample_sums(subsetG)
+sample_variables(subsetG)
+
+install.packages("scales")
+
+plot_phyloseq(subsetG)
+sample_variables(subsetG)
+
+sample_data(subsetG)$Cluster = as.factor(sample_data(subsetG)$Cluster)
+
+
+plot_tree(subsetG, "treeonly", nodeplotblank)
+
+plot_net(subsetG, maxdist= 0.82, color ="Cluster", point_label = "Sname")
+
+for (i in sample_variables(subsetG)){
+  #print(unique(sample_data(subsetG)$i))
+  print(i)
+}
+
+
+unique(sample_data(subsetG)$Cox)
+
+nw <- make_network(subsetG, max.dist=.82)
+plot_network(nw, subsetG, color = "Cluster")
+
+
+
+
+pseq <- read_phyloseq(otu.file= "ASV.biom1",
+                      taxonomy.file = NULL,
+                      metadata.file = "MetaData.csv",
+                      type="biom", sep =";" )
